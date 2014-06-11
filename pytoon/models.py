3a66a9afda7aab389f872b07856d8123f8432569
Marco Plaisier <marco.plaisier@finalist.com>
2014-06-11 20:49:06 +0200

    Maybe this then

--- pytoon/models.py
+++ pytoon/models.py
@@ -2,10 +2,8 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 
